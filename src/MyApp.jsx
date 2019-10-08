af45944f2f21d4c287ed3b9a907a1a260a173b0c
sosno piotr <sosnop@p21702w.MINI>
2019-10-08 10:08:13 +0200

    Customize headline

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const MyApp = () => (
-  <h1>Minimal React Hehe</h1>  
+  <h1>Minimal React sosna252 7b7dbc59cafab9702e77ffc40699bd4d56e85c50</h1>  
 )
 
 export default MyApp
