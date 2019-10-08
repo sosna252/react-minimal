f2ae5b9d320abd570dfb5590e4841082ff9615b7
sosno piotr <sosnop@p21702w.MINI>
2019-10-08 10:09:25 +0200

    New feature

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 const MyApp = () => (
-  <h1>Minimal React sosna252 7b7dbc59cafab9702e77ffc40699bd4d56e85c50</h1>  
+  <h1>Minimal React sosna252 7b7dbc59cafab9702e77ffc40699bd4d56e85c50
+    <p>Bundle size: X bytes, Load time of the bundle: Y ms, Last commit SHA1: Z</p> 
+  </h1> 
+  
 )
 
 export default MyApp
