774c8135e7bb75a96b0665a6fd77772bc1356fc9
Piotr <sosna252@gmail.com>
2019-10-11 14:52:43 +0200

    Test

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const MyApp = () => (
-  <h1>Minimal React sosna252 7b7dbc59cafab9702e77ffc40699bd4d56e85c50
+  <h1>Minimal React sosna252
     <p>Bundle size: X bytes, Load time of the bundle: Y ms, Last commit SHA1: Z</p> 
   </h1> 
   
