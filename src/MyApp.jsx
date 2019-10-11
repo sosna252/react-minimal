9da3d89d44e964c94f6fb336940d1fb47fc107e7
Piotr <sosna252@gmail.com>
2019-10-11 23:21:51 +0200

    Production bundle analysis

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
 const MyApp = () => (
-  <h1>Minimal React sosna252
-    <p>Bundle size: 122 bytes, Load time of the bundle: Y ms, Last commit SHA1: Z</p> 
-  </h1> 
-  
+  <body>
+  <h1>Minimal React sosna252</h1> 
+  <p>Bundle size: 129 bytes, Load time of the bundle: 248 ms, Last commit SHA1: 962036334d2613fad275bc37cd2eb01c1f99b6ba</p> 
+  </body>
 )
 
 export default MyApp
