0f8395805c5f7497bc569882718e10c3a7293dd9
Piotr <sosna252@gmail.com>
2019-10-12 19:19:18 +0200

    Lab1Ex3

--- src/AppTitle.js
+++ src/AppTitle.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const generateArray = (n) => Array.from({length: n}, (v, k) => k+1);
+const generateRandomArray = (n) => Array.from({length: n}, (v, k) => Math.round(Math.random()*24 +1 ));
 
 const AppTitle = () => (   
-  "Lab1.ex1 = " + generateArray(10).toString()
+  "Lab1.ex3 = " + generateRandomArray(10).toString()
    )
 
 console.log(generateArray(10))
+console.log(generateRandomArray(10))
 
 export default AppTitle
